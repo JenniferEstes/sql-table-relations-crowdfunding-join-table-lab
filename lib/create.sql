578959ee4feb41a948b36ea02ba677b7e712de2b
Jennifer <jenndestes@gmail.com>
2021-03-03 20:10:01 -0600

    commit tree 01aad37a0c4d445ca0044a931ddd90e170cd6f49

---
+++ lib/create.sql
@@ -0,0 +1,27 @@
+-- * A project has a title, a category, a funding goal, a start date, and an end date.
+-- * A user has a name and an age
+-- * A pledge has an amount. It belongs to a user,
+
+CREATE TABLE projects (
+    id INTEGER PRIMARY KEY,
+    title TEXT, 
+    category TEXT,
+    funding_goal NUMERIC,
+    start_date TEXT, 
+    end_date TEXT
+);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount NUMERIC, 
+name TEXT,
+age INTEGER,
+project_id INTEGER,
+user_id INTEGER 
+);
